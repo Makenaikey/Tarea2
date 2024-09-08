2b96869643be96b1155cd9cf96f073f2a094c69c
Kevins <kevin.miranda2101@alumnos.ubiobio.cl>
2024-09-08 14:39:07 -0300

    ADD se agrega Variables y Tipos de Datos

---
+++ Variables y Tipos de Datos.js
@@ -0,0 +1,15 @@
+//Declaracion de la variable nombre
+var nombre = 'Kevin';
+//Declaracion de la variable edad
+var edad = 25;
+//Declaracion de la variable puedo conducir
+var puedoConducir = true;
+//Imprimir datos
+console.log("Nombre:", nombre);
+console.log("Edad:", edad);
+console.log("¿Puedo Conducir?", puedoConducir);
+
+//Imprimir tipos de datos
+console.log("Tipo de dato de `nombre`:", typeof nombre);
+console.log("Tipo de dato de `edad`:", typeof edad);
+console.log("Tipo de dato de `puedoConducir`:", typeof puedoConducir);
