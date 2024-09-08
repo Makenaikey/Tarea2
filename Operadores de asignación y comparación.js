34057bf657ef84d6cc3cd8a4225a491483e2bb5d
Kevins <kevin.miranda2101@alumnos.ubiobio.cl>
2024-09-08 15:07:18 -0300

    se arregla

---
+++ Operadores de asignación y comparación.js
@@ -0,0 +1,22 @@
+//Declaracion de las Constantes iniciales
+const numero1 = 15;
+const numero2 = 20;
+const numero3 = '25';
+
+// 1. Mostrar por consola si el numero1 es mayor o igual que numero2.
+console.log("numero1 es mayor o igual que numero2?", numero1 >= numero2);
+
+// 2. Mostrar por consola si el numero1 es menor o igual que numero2.
+console.log("numero1 es menor o igual que numero2?", numero1 <= numero2);
+
+// 3. Mostrar por consola si el numero1 es menor que numero3.
+console.log("numero1 es menor que numero3?", numero1 < numero3);
+
+// 4. Mostrar por consola si el numero3 es menor que numero2.
+console.log("numero3 es menor que numero2?", numero3 < numero2);
+
+// 5. Mostrar por consola si el numero3 es estrictamente diferente que numero1.
+console.log("numero3 es estrictamente diferente que numero1?", numero3 !== numero1);
+
+// 6. Mostrar por consola si el numero1 es estrictamente igual al numero2.
+console.log("numero1 es estrictamente igual al numero2?", numero1 === numero2);
